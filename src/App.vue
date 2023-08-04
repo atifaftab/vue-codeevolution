83ed1e93ace354c8e795be36279be7dfc9ca095b
Atif <iamatif96@gmail.com>
2023-08-04 09:57:03 +0400

    classes added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" /> -->
+    <div><h2>{{intro}} {{ name }}</h2></div>
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <button class="button" v-bind:disabled="isDisabled" >Enter</button>
+
+    <h2 v-bind:id="headingId">Welcome to Batcave</h2>
+    <h2 class="underline">underlined</h2>
+    <h2 class="underline" v-bind:class="status">Status</h2>
+    <h2 v-bind:class="isPromoted && 'promoted'">Movie</h2>
+    <h2 v-bind:class="isSoldOut ? 'sold-out' : 'new'">new releases</h2>
+    <h2 v-bind:class="['new','promoted']">I am green and Italic</h2>
+    <h2 v-bind:class="[isPromoted && 'promoted',isSoldOut ? 'sold-out' : 'new']">Array with conditional  </h2>
+    <h2 v-bind:class="
+    {
+      promoted : isPromoted,
+      new : !isSoldOut,
+      'sold-out' : isSoldOut
+    }" >Object conditions</h2>
 </template>
 
 <script>
@@ -8,8 +25,39 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return{
+      name : 'Atif',
+      intro : 'I am',
+      headingId : 'Welcome to Batcave',
+      isDisabled : false,
+      status : 'danger',
+      isPromoted : true,
+      isSoldOut : true
+    };
+  },
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
+<style>
+#app{
+
+}
+.button{
+  text-align: center;
+}
+.underline {
+  text-decoration: underline;
+}
+.promoted {
+  font-style: italic;
+}
+.new{
+  color: green;
+}
+.sold-out{
+  color: red;
+}
+</style>
