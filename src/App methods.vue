ff21de0b2332fa59b9ccbd8e3f30d22c40252ce9
Atif <iamatif96@gmail.com>
2023-08-07 12:16:38 +0400

    event handling

---
+++ src/App methods.vue
@@ -0,0 +1,24 @@
+<template>
+<h2>{{ add(2,1,1) }}</h2>
+<h2>{{ multiply(baseValue) }}</h2>
+</template>
+
+<script>
+    export default{
+        name : 'App',
+        data(){
+            return{
+                baseValue : 10 ,
+                baseMultiplier : 5
+            }
+        },
+        methods: {
+            add(a,b,c){
+                return (a+b+c)*this.baseValue;
+            },
+            multiply(a){
+                return a*this.baseMultiplier;
+            }
+        }
+    }
+</script>
