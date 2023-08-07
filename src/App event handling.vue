69aadd9f132c0542edd5934deb853cf82186cdfb
Atif <iamatif96@gmail.com>
2023-08-07 13:06:52 +0400

    event handling done

---
+++ src/App event handling.vue
@@ -0,0 +1,35 @@
+<template>
+<h2>{{ name }}</h2>
+<!-- <button v-on:click="name = 'Batman'">click to change</button> -->
+<button v-on:click="changeName">click to change</button>
+
+<h2>{{ count }}</h2>
+<button v-on:click="increment(1)">increase 1</button>
+<button v-on:click="increment(5)">increase 5</button>
+<button v-on:click="decrement(1)">decrease 1</button>
+<button v-on:click="decrement(5)">decrease 5</button>
+<!-- <button v-on:click="count-=1">decrease</button> -->
+</template>
+
+<script>
+   export default{
+      name: 'App',
+      data(){
+         return{
+            name : 'Atif',
+            count : 0
+         }
+      },
+      methods : {
+         increment(val){
+            return this.count+=val;
+         },
+         decrement(val){
+            return this.count-=val;
+         },
+         changeName(){
+            return this.name = 'Batman';
+         }
+      }
+   }
+</script>
