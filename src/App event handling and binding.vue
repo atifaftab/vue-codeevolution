f6724423f1d71412bfdc886a18aa03b3629f6b4e
Atif <iamatif96@gmail.com>
2023-08-07 14:05:40 +0400

    event handling and biding

--- src/App event handling and binding.vue
+++ src/App event handling and binding.vue
@@ -1,13 +1,13 @@
 <template>
 <h2>{{ name }}</h2>
 <!-- <button v-on:click="name = 'Batman'">click to change</button> -->
-<button v-on:click="changeName">click to change</button>
+<button @click ="changeName, increment(1, $event)">click to change</button>
 
 <h2>{{ count }}</h2>
-<button v-on:click="increment(1)">increase 1</button>
-<button v-on:click="increment(5)">increase 5</button>
-<button v-on:click="decrement(1)">decrease 1</button>
-<button v-on:click="decrement(5)">decrease 5</button>
+<button @click="increment(1, $event)">increase 1</button>
+<button @click="increment(5)">increase 5</button>
+<button @click="decrement(1)">decrease 1</button>
+<button @click="decrement(5)">decrease 5</button>
 <!-- <button v-on:click="count-=1">decrease</button> -->
 </template>
 
@@ -21,13 +21,15 @@
          }
       },
       methods : {
-         increment(val){
+         increment(val,event){
+            console.log(event)
             return this.count+=val;
          },
          decrement(val){
             return this.count-=val;
          },
-         changeName(){
+         changeName(event){
+            console.log(event)
             return this.name = 'Batman';
          }
       }
