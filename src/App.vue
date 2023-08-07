d5675afe7fc4fbbda030411db8908fb37042e9db
Atif <iamatif96@gmail.com>
2023-08-07 10:34:08 +0400

    various array within object and array

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
 <h2 v-for="(name, index) in names">{{index}} {{ name }}</h2>
 <h2 v-for="(name) in fullnames">{{ name.firstName }} {{ name.lastName }}</h2>
+
+<div v-for="actor in actors">
+<h2>{{ actor.name }}</h2>
+<h3 v-for="movie in actor.movies">{{ movie }} </h3>
+</div>
 </template>
 <script>
 import { createApp } from 'vue';
@@ -14,11 +19,9 @@ export default{
                 {firstName: 'Bruce', lastName: 'Wayne'},
                 {firstName: 'Clark', lastName: 'Kent'},
                 {firstName: 'Barry', lastName: 'Allen'},
-
             ],
-            actors : [                        //array of object, each object contains 
-                                              // name and movies , movies is an array
-                {
+            actors : [                        //array of object, each object contains
+                {                              // name and movies , movies is an array
                     name : 'Ben Affleck', 
                     movies : ['bvs', 'jl']
                 },
