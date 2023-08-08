048b7869bab6cb93e9911606610dabc7e87cc821
Atif <iamatif96@gmail.com>
2023-08-08 10:09:46 +0400

    form handling with v binding

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
 <template>
+<div>
+    <pre>
+        {{ JSON.stringify(formValues,null,2) }}
+    </pre>
+</div>
+
+    <form>
+        <div>
+            <label for="name">Name</label>
+            <input type="text" id="name" v-model="formValues.name">
+        </div>
+        <div>
+            <label for="profileSummary">Profile Summary</label>
+            <textarea id="profileSummary" v-model="formValues.profileSummary" ></textarea>
+        </div>
+    </form>
 
 </template>
 <script>
 export default{
     name: 'App',
     data(){
+        return{
+            formValues:{
+                name:'',
+                profileSummary:''
+            }
+        }
 
     },
     methods:{
@@ -12,3 +34,52 @@ export default{
     }
 }
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.underline {
+  text-decoration: underline;
+}
+.promoted {
+  font-style: italic;
+}
+.new {
+  color: olivedrab;
+}
+.sold-out {
+  color: red;
+}
+label {
+  font-weight: bold;
+  display: flex;
+  margin-bottom: 5px;
+}
+
+input + label {
+  font-weight: bold;
+  display: inline-flex;
+  margin-right: 20px;
+}
+
+input[type='text'],
+textarea,
+select {
+  display: block;
+  width: 400px;
+  padding: 6px 12px;
+  font-size: 14px;
+  line-height: 1.42857143;
+  color: #555;
+  background-color: #fff;
+  background-image: none;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+</style>
