7db02f90dd81f88b426b96ff6c9b2889ad060cd0
Atif <iamatif96@gmail.com>
2023-08-09 13:14:06 +0400

    immediate and deep watcher

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,12 @@
   <h2>Current Volume - {{ volume }}</h2>
   <button @click="volume += 2"> + </button>
   <button @click="volume -= 2"> - </button>
+
+  <input type="text" v-model="movie">
+  <input type="text" v-model="movieInfo.title">
+  <input type="text" v-model="movieInfo.info">
+  <button @click="movieList.push('flash')">Add Movie</button>
+
 </template>
 
 <script>
@@ -10,7 +16,13 @@ export default {
   name: 'App.vue',
   data() {
     return {
-      volume: 0
+      volume: 0,
+      movie: 'Batman',
+      movieInfo :{
+        title : '',
+        info : ''
+      },
+      movieList : ['superman', 'Batman','Thor']
     }
   },
   methods: {
@@ -24,7 +36,42 @@ export default {
       if (value > lastValue && value == 16) {
         alert("higher volume can damage hearing")
       }
+    },
+    movie: {
+      handler(newValue) {
+        console.log("new Movie : ", newValue)
+      },
+      immediate : true
+    },
+    movieInfo:{
+      handler(newValue){
+        console.log(`The Title of movie ${newValue.title} and info ${newValue.info}`)
+      },
+      deep : true
+    },
+    movieList :{
+      handler(newValue){
+        console.log(`the new movie List ${newValue}`)
+      },
+      deep : true
     }
   }
 }
 </script>
+<style>
+.button{
+  text-align: center;
+}
+.underline {
+  text-decoration: underline;
+}
+.promoted {
+  font-style: italic;
+}
+.new{
+  color: green;
+}
+.sold-out{
+  color: red;
+}
+</style>
